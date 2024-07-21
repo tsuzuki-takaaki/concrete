e46e3ac3e66a1cb7d61f814de44914c39b86b91c
tsuzuki-takaaki <echo.tsuzuki.takaaki@gmail.com>
2024-07-21 20:10:27 +0900

    Create method for each query

--- src/mysql2/mod.rb
+++ src/mysql2/mod.rb
@@ -6,17 +6,39 @@ MYSQL_DATABASE = ENV["MYSQL_DATABASE"]
 MYSQL_USER = ENV["MYSQL_USER"]
 MYSQL_PASSWORD = ENV["MYSQL_PASSWORD"]
 
-mysql_client = Mysql2::Client.new(
-  :host => MYSQL_HOST,
-  :database => MYSQL_DATABASE,
-  :username => MYSQL_USER,
-  :password => MYSQL_PASSWORD
-)
+# TODO: Create connection pool
+def client
+  Mysql2::Client.new(
+    :host => MYSQL_HOST,
+    :database => MYSQL_DATABASE,
+    :username => MYSQL_USER,
+    :password => MYSQL_PASSWORD
+  )
+end
 
 # ----------
 # Mysql2::Result(returned from issuing a #query on the connection. It includes Enumerable.)
 # Not Collection but Iterator
 # to_a -> [{"COLUMN_NAME"=>"VALUE"}, ...]
+# ----------
+
+SCHEMA_FILE_PATH = File.expand_path("../../../sql/mysql/schema.sql", __FILE__)
+SEED_FILE_PATH = File.expand_path("../../../sql/mysql/seed.sql", __FILE__)
+def mysql_initialize
+  out, status = Open3.capture2("mysql -u#{MYSQL_USER} -p#{MYSQL_PASSWORD} -h#{MYSQL_HOST} #{MYSQL_DATABASE} < #{SCHEMA_FILE_PATH}")
+  unless status.success?
+    puts "Failed to initialize"
+    exit
+  end
+
+  out, status = Open3.capture2("mysql -u#{MYSQL_USER} -p#{MYSQL_PASSWORD} -h#{MYSQL_HOST} #{MYSQL_DATABASE} < #{SEED_FILE_PATH }")
+  unless status.success?
+    puts "Failed to initialize"
+    exit
+  end
+
+  return nil
+end
 
 # ---------- show databases
 #  SHOW DATABASES;
@@ -29,15 +51,16 @@ mysql_client = Mysql2::Client.new(
 #  | performance_schema |
 #  +--------------------+
 #  3 rows in set (0.01 sec)
-
 ShowDatabaseRow = Struct.new(:Database, keyword_init: true)
-# return @Mysql2::Result
-mysql_client.query("SHOW DATABASES;").each do |row|
-  sdb = ShowDatabaseRow.new(row)
-  puts sdb
+def show_databases
+  # @Mysql2::Result
+  result = client.query("SHOW DATABASES;")
+  result.each do |row|
+    sdb = ShowDatabaseRow.new(row)
+    puts sdb
+  end
+  puts result.to_a
 end
 
-# ---------- (create|drop) schema
-SCHEMA_FILE_PATH = File.expand_path("../../../sql/mysql/schema.sql", __FILE__)
-out, status = Open3.capture2("mysql -u#{MYSQL_USER} -p#{MYSQL_PASSWORD} -h#{MYSQL_HOST} #{MYSQL_DATABASE} < #{SCHEMA_FILE_PATH}")
-puts out, status
+mysql_initialize
+show_databases
