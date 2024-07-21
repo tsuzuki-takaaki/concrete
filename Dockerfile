15c5cf06b89daf84611e12dccf2c04f212a6b9f6
tsuzuki-takaaki <echo.tsuzuki.takaaki@gmail.com>
2024-07-21 13:34:00 +0900

    0x09 -> 2 * 0x20

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM ruby:3.2-bookworm
 
 # This packages is not needed, just for searching in the container
 RUN apt update -y && apt install -y \
-		vim \
-		netcat-openbsd \
-		default-mysql-client
+    vim \
+    netcat-openbsd \
+    default-mysql-client
 
 WORKDIR /app
 COPY /Gemfile ./Gemfile
--- sql/mysql/schema.sql
+++ sql/mysql/schema.sql
@@ -3,18 +3,18 @@ USE `concrete`;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`name` VARCHAR(255) NOT NULL,
-	`email` VARCHAR(255) NOT NULL,
-	PRIMARY KEY (`id`)
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NOT NULL,
+  `email` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
 DROP TABLE IF EXISTS `post`;
 CREATE TABLE `post` (
-	`id` INT NOT NULL AUTO_INCREMENT,
-	`tittle` VARCHAR(255) NOT NULL,
-	`content` TEXT NOT NULL,
-	`user_id` INT NOT NULL,
-	PRIMARY KEY (`id`),
-	INDEX `idx_userid` (`user_id`)
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `tittle` VARCHAR(255) NOT NULL,
+  `content` TEXT NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `idx_userid` (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
